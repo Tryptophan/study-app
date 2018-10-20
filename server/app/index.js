9cb8028102ae1ce4a8a3d4c9a66a6ca9c93e6f6c
Jacob Greenway <jagreenway12@gmail.com>
2018-10-20 18:36:17 -0500

    add enpoints for chat, login, and courses

--- server/app/index.js
+++ server/app/index.js
@@ -7,22 +7,41 @@ import mongo from './mongo';
 mongo.connect((err) => {
   if (err) throw err;
 
+  // Init REST and socket server
   const app = express();
   const server = http.Server(app);
   const io = socketio(server);
 
   // Client has connected to socket server
   io.on('connection', (socket) => {
+
     // Client sent chat message
     socket.on('chat', (chat) => {
       console.log(chat);
-      // TODO: Insert chat into mongodb course collection
-      // TODO: Send chat back to users in the course room
+
+      // Add timestamp to chat
+      chat.timestamp = Date.now();
+
+      // Get all rooms socket is in
+      for (let room in socket.rooms) {
+        if (room !== socket.id) {
+          // Insert chat into mongodb course collection
+          mongo.getDb().collection('courses').findOneAndUpdate({ id: room }, {
+            '$push': { chat: chat }
+          }, { returnOriginal: false }, (err, res) => {
+            // Send chat back to users in the course room
+            io.to(room).emit('chat', chat);
+          });
+        }
+      }
     });
+
   });
 
+  // Body parser
   app.use(bodyParser.json());
 
+  // CORS Requests
   app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, PATCH, OPTIONS');
@@ -30,10 +49,33 @@ mongo.connect((err) => {
     next();
   });
 
-  app.get('/', (req, res) => {
-    res.send('');
+  // TODO: Log user in using blackboard credentials
+  app.get('/login', (req, res) => {
+    // TODO: Get username and password from request
+    // TODO: Send token back to client for further requests
+    res.status(500);
   });
 
+  // Get courses for logged in user
+  app.get('/courses', (req, res) => {
+    // TODO: Get courses from blackboard with token
+    res.json([
+      {
+        id: '123456',
+        name: 'Advanced Algorithms',
+      },
+      {
+        id: '123423y123',
+        name: 'Databases'
+      },
+      {
+        id: 'wqehqweqwe',
+        name: 'Programming Languages'
+      }
+    ]);
+  });
+
+  // Listen on port
   server.listen(process.env.PORT, () => {
     console.log('Listening on http://localhost:%s', process.env.PORT);
   });
