eb7b6c565b5eb49a81d449aa1f3f0c321715ee23
Jacob Greenway <jagreenway12@gmail.com>
2018-10-21 04:44:57 -0500

    react chat

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Switch, Route } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
+
+import 'font-awesome/css/font-awesome.min.css';
+import 'bulma/css/bulma.min.css';
 import './App.css';
 
+import Chat from './Chat';
+
+import axios from 'axios';
+
 class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='App'>
+        <Switch>
+          {/* <Route exact path='/' component={null} /> */}
+          <Route path='/chat/:courseId' component={Chat} />
+        </Switch>
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
---
+++ client/src/Chat.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import { Scrollbars } from 'react-custom-scrollbars';
+
+import socket from './socket';
+
+import './Chat.css';
+
+class Chat extends Component {
+
+  constructor(props) {
+    super(props);
+
+    socket.on('chat', this.onChat);
+
+    this.state = {
+      chats: []
+    };
+  }
+
+  render() {
+
+    let chats = this.state.chats.map((chat) =>
+      <div key={chat.timestamp}>{chat.message}</div>
+    );
+
+    return (
+      <div className='Chat'>
+        <div className='ChatTitle'>Title</div>
+        {/* Render all previous chats and incoming */}
+        <Scrollbars ref={el => { this.scroll = el }}><div className='Messages'>{chats}</div></Scrollbars>
+        <div className='ChatControls'>
+          <div className='field has-addons'>
+            <div className='control Input'>
+              <input className='input' ref={el => { this.input = el }} onKeyPress={this.onKeyPress} type='text' placeholder='Type here!' />
+            </div>
+            <div className='control'>
+              <button onClick={this.send} className='button'><i className='fa fa-send' /></button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    // Join the chat room
+    let { courseId } = this.props.match.params;
+    socket.emit('join', courseId);
+  }
+
+  componentDidUpdate() {
+    this.scroll.scrollToBottom();
+  }
+
+  onKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      this.send();
+    }
+  }
+
+  send = () => {
+    let message = this.input.value;
+
+    socket.emit('chat', {
+      message: message
+    });
+
+    this.input.value = null;
+  }
+
+  onChat = (chat) => {
+    this.setState({
+      chats: this.state.chats.concat(chat)
+    });
+  }
+}
+
+export default Chat;
